626170f8a5312e8227cfc0ea472aec3406be3b15
candace <candace.marzec@gmail.com>
2019-01-24 10:58:23 -0800

    adds validations

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -13,21 +13,33 @@ class Api::ContactsController < ApplicationController
   def create
     @contact = Contact.new(
     first_name: params["first_name"],
-    last_name: params["last_name"], 
+    last_name: params["last_name"],
+    middle_name: params["middle_name"], 
     email: params["email"],
-    phone_number: params["phone_number"])
-    @contact.save
-    render 'show.json.jbuilder'   
+    phone_number: params["phone_number"],
+    bio: params["bio"])
+    
+    if @contact.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+    end       
   end 
 
   def update
     @contact = Contact.find_by(id: params["id"])
     @contact.first_name = params["first_name"] || @contact.first_name
+    @contact.middle_name = params["middle_name"] || @contact.middle_name
     @contact.last_name = params["last_name"] || @contact.last_name
     @contact.email = params["email"] || @contact.email
     @contact.phone_number = params["phone_number"] || @contact.phone_number
-    @contact.save
-    render 'show.json.jbuilder'
+    @contact.bio = params["bio"] || @contact.bio
+     
+    if @contact.save
+      render 'show.json.jbuilder'
+    else
+      render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+    end  
   end
 
   def destroy
